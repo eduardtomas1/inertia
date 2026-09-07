8e5753dc7fb3609301ea90920e43c1a89a7afa11
Eduard Tomas <etomasync@gmail.com>
2026-09-07 18:08:27 +0200

    test: declare primary display for provider picker geometry

--- tests/e2e/provider-settings-visual.spec.ts
+++ tests/e2e/provider-settings-visual.spec.ts
@@ -1,3 +1,4 @@
+// @inertia-e2e-resource primary-display
 import { expect, test, type Page, type TestInfo } from "@playwright/test";
 
 import { createAppFixture, type AppFixture } from "./support/app-fixture";
